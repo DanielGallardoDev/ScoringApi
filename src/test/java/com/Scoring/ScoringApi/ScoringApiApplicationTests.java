2cf053d65647f6c2acdaa1578b242be77fbfb173
Daniel Gallardo <daniel.gallardo@babelgroup.com>
2023-03-20 12:43:17 +0100

    Initial commit

---
+++ src/test/java/com/Scoring/ScoringApi/ScoringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Scoring.ScoringApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
